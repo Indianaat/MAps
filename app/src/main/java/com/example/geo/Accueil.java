1c5364150d22e952898cdf3728bc3c1ece5e700a
Indianaat <manonloupio@gmail.com>
2021-04-03 20:07:07 +0200

    Création du projet

---
+++ app/src/main/java/com/example/geo/Accueil.java
@@ -0,0 +1,42 @@
+package com.example.geo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Accueil extends AppCompatActivity {
+
+    EditText login, password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+        login = findViewById(R.id.login);
+        password = findViewById(R.id.password);
+
+    }
+    public void actQuitter(View v){
+        finish();
+    }
+    public void actEffacer(View v){
+        login.setText("");
+        password.setText("");
+    }
+    public void actValider(View v){
+        if(login.getText().toString().equals(getResources().getString(R.string.login)) &&
+                password.getText().toString().equals(getResources().getString(R.string.password))){
+            Toast.makeText(getApplicationContext(),getResources().getString(R.string.logok),Toast.LENGTH_LONG).show();
+            //Envoi de l'intent à l'activity accueil
+            Intent i= new Intent(Accueil.this,MainActivity.class);
+            i.putExtra("msg",login.getText().toString());
+            //lancement de l'activity Accueil
+            startActivity(i);
+        }
+        else
+            Toast.makeText(getApplicationContext(),getResources().getString(R.string.logfailed),Toast.LENGTH_LONG).show();
+    }
+}
