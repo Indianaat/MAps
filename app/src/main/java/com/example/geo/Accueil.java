3f8858ac5ecd426cb0de043d80380f39c67ef6f6
Indianaat <manonloupio@gmail.com>
2021-04-04 22:05:41 +0200

    Ajout des page: Connexion, Accueil, Quizz, Score et Geo, Modification du logo

--- app/src/main/java/com/example/geo/Accueil.java
+++ app/src/main/java/com/example/geo/Accueil.java
@@ -5,38 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 public class Accueil extends AppCompatActivity {
 
-    EditText login, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-        login = findViewById(R.id.login);
-        password = findViewById(R.id.password);
-
     }
-    public void actQuitter(View v){
-        finish();
-    }
-    public void actEffacer(View v){
-        login.setText("");
-        password.setText("");
-    }
-    public void actValider(View v){
-        if(login.getText().toString().equals(getResources().getString(R.string.login)) &&
-                password.getText().toString().equals(getResources().getString(R.string.password))){
-            Toast.makeText(getApplicationContext(),getResources().getString(R.string.logok),Toast.LENGTH_LONG).show();
-            //Envoi de l'intent à l'activity accueil
-            Intent i= new Intent(Accueil.this,MainActivity.class);
-            i.putExtra("msg",login.getText().toString());
-            //lancement de l'activity Accueil
+    public void actQuiz(View v){
+            //Envoi de l'intent à l'activity quiz
+            Intent i= new Intent(Accueil.this,Quiz.class);
+            //lancement de l'activity Quiz
             startActivity(i);
-        }
-        else
-            Toast.makeText(getApplicationContext(),getResources().getString(R.string.logfailed),Toast.LENGTH_LONG).show();
+    }
+    public void actGeo(View v){
+        //Envoi de l'intent à l'activity Geo
+        Intent i= new Intent(Accueil.this,Geo.class);
+        //lancement de l'activity Geo
+        startActivity(i);
+    }
+    public void actScore(View v){
+        //Envoi de l'intent à l'activity Score
+        Intent i= new Intent(Accueil.this,Score.class);
+        //lancement de l'activity Score
+        startActivity(i);
     }
 }
